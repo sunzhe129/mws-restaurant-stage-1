77b9ee9e403b2f7cefdd580fa8414e416ef93eb3
sunzhe129 <sunzhe_129@aliyun.com>
2019-03-16 21:56:33 +0800

    Add files via upload

---
+++ mws-restaurant-stage-1/sw.js
@@ -0,0 +1,43 @@
+var staticCacheName = 'mws-restaurant-static-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/sw.js',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
