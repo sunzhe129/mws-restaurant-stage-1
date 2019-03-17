8916024e9294a45922adf383a9f3cc3a2051d1f5
sunzhe129 <sunzhe_129@aliyun.com>
2019-03-17 20:23:35 +0800

    Add files via upload

--- mws-restaurant-stage-1/js/main.js
+++ mws-restaurant-stage-1/js/main.js
@@ -176,9 +176,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", "An image of " + restaurant.name + " restaurant");
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
